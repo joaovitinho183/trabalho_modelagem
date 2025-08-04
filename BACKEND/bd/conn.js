c0ae3fd2853d97abdf7f5776fb07dce46f70505e
JOÃO VITOR SANTOS <joao_santos52@estudante.sesisenai.org.br>
2025-08-04 09:55:09 -0300

    final2

--- BACKEND/bd/conn.js
+++ BACKEND/bd/conn.js
@@ -1,8 +1,8 @@
 const { Sequelize } = require('sequelize')
 require('dotenv').config()
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
-    host: process.env.DB_HOST,
+const sequelize = new Sequelize('db_compras', 'root', 'senai', {
+    host: 'localhost',
     dialect: 'mysql',
     port: 3306
 })
